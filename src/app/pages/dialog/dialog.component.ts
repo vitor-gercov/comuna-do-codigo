341fab880cef0159d5e506f8cbc7ee4035854f5c
vitor-gercov <gercovvitor@gmail.com>
2025-09-04 07:44:08 -0300

    implementação da opção de bottomsheet na tela de dialog

--- src/app/pages/dialog/dialog.component.ts
+++ src/app/pages/dialog/dialog.component.ts
@@ -1,23 +1,103 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, model } from '@angular/core';
 import { CodeProviderComponent } from '@components';
 import { CodeProvider, UsefulLinksList } from '@types';
 import { UsefulLinksListComponent } from 'app/components/useful-links-list/useful-links-list.component';
 import { ComunaMainButtonDirective } from "@directives";
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dialog',
   imports: [
     CodeProviderComponent,
     UsefulLinksListComponent,
-    ComunaMainButtonDirective
+    ComunaMainButtonDirective,
+    NgClass,
+    FormsModule
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
 export class DialogComponent {
-  code = signal<CodeProvider>({
-    html:
-      `
+  dialogTypeSelected = model<string>('dialog');
+  css = computed<string>(() => {
+    let css = '';
+    switch (this.dialogTypeSelected()) {
+      case 'dialog':
+        css =
+          `
+          dialog {
+            width: 100%;
+            max-width: 1270px;
+            border-radius: 1rem;
+
+            &::backdrop {
+              background-color: rgba(0, 0, 0, .5);
+            }
+
+            header {
+              display: flex;
+              justify-content: space-between;
+
+              button {
+                border: none;
+                height: 3rem;
+                width: 3rem;
+                border-radius: 50%;
+                cursor: pointer;
+                padding: 0;
+              }
+            }
+          }
+          `;
+        break;
+      case 'bottomsheet':
+        css =
+          `
+          dialog {
+            width: 100%;
+            max-width: 1270px;
+            border-radius: 1rem;
+
+            &::backdrop {
+              background-color: rgba(0, 0, 0, .5);
+            }
+
+            header {
+              display: flex;
+              justify-content: space-between;
+
+              button {
+                border: none;
+                height: 3rem;
+                width: 3rem;
+                border-radius: 50%;
+                cursor: pointer;
+                padding: 0;
+              }
+            }
+          }
+
+          .bottomsheet {
+            margin-bottom: 0;
+            border-radius: 1rem 1rem 0 0;
+            transform: translateY(0);
+            transition: transform .25s ease-in;
+
+            @starting-style {
+              transform: translateY(100%);
+            }
+          }
+          `;
+        break;
+    }
+    return css;
+  });
+  code = computed<CodeProvider>(() => {
+    return {
+      html:
+        `
       <button id="open_dialog_button">
         Abrir dialog
       </button>
@@ -33,8 +113,8 @@ export class DialogComponent {
         </header>
       </dialog>
       `,
-    js:
-      `
+      js:
+        `
       const dialog = document.querySelector('dialog');
       const openDialogButton = document.querySelector('#open_dialog_button');
       const openDialogButton = document.querySelector('#close_dialog_button');
@@ -46,33 +126,9 @@ export class DialogComponent {
         dialog.close();
       });
       `,
-    css:
-      `
-      dialog {
-        width: 100%;
-        max-width: 1270px;
-        border-radius: 1rem;
-
-        &::backdrop {
-          background-color: rgba(0, 0, 0, .5);
-        }
-
-        header {
-          display: flex;
-          justify-content: space-between;
-
-          button {
-            border: none;
-            height: 3rem;
-            width: 3rem;
-            border-radius: 50%;
-            cursor: pointer;
-            padding: 0;
-          }
-        }
-      }
-      `
-  })
+      css: this.css()
+    }
+  });
   usefulLinksList: UsefulLinksList = [
     {
       text: 'Disponibilidade do elemento (Can I Use)',
@@ -81,6 +137,22 @@ export class DialogComponent {
     {
       text: 'Documentação oficial do elemento (MDN)',
       link: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog'
-    }
+    },
+    {
+      text: 'Video-aula sobre dialogs por Kevin Powell (en) (Youtube)',
+      link: 'https://www.youtube.com/watch?v=TAB_v6yBXIE&ab_channel=KevinPowell'
+    },
+    {
+      text: 'Artigos sobre o elemento dialog (CSS Tricks)',
+      link: 'https://css-tricks.com/tag/dialog/'
+    },
+    {
+      text: 'Documentação oficial sobre @starting-style (MDN)',
+      link: 'https://developer.mozilla.org/en-US/docs/Web/CSS/@starting-style'
+    },
+    {
+      text: 'Artigo sobre @starting-style por Juan Diego Rodríguez (CSS Tricks)',
+      link: 'https://css-tricks.com/almanac/rules/s/starting-style/'
+    },
   ];
 }
